96d5aef35386344dff7a4e99f4b804c88ea0651d
Jo Colina <jo@jocolina.com>
2024-02-17 10:49:55 +0100

    GitHub: deprecate ::save-state and ::set-output in favor of environment files

--- test/main.js
+++ test/main.js
@@ -8,23 +8,28 @@ const Docker = require('../lib/docker');
 
 describe('Main script', () => {
 	let exit_stub;
-	let github_stub;
+	let github_state_stub;
+	let github_output_stub;
+
 	beforeEach(() => {
 		// Do this here to reset actual stub every time, otherwise node keeps this in cache
 		delete require.cache[require.resolve('../scripts/main')];
 		exit_stub = Sinon.stub(ProcessManager, 'exit');
-		github_stub = Sinon.stub(GitHub, 'command');
+		github_state_stub = Sinon.stub(GitHub, 'save_state');
+		github_output_stub = Sinon.stub(GitHub, 'set_output');
 	});
 
 	afterEach(() => {
 		exit_stub.restore();
-		github_stub.restore();
+		github_state_stub.restore();
+		github_output_stub.restore();
 	});
 
 	it('Should run pull/build commands without special cache', done => {
+		// This is the difference between both tests
+		process.env.INPUT_COMPUTE_DOCKERFILE_HASH = 'false';
 		process.env.INPUT_DOCKERFILE_PATH = 'INPUT_DOCKERFILE_PATH';
 		process.env.INPUT_INTERMEDIARY_HASH = 'INPUT_INTERMEDIARY_HASH';
-		process.env.INPUT_COMPUTE_DOCKERFILE_HASH = 'false';
 		process.env.INPUT_INTERMEDIARY_IMAGE_AS = 'INPUT_INTERMEDIARY_IMAGE_AS';
 		process.env.INPUT_INTERMEDIARY_IMAGE_URL = 'INPUT_INTERMEDIARY_IMAGE_URL';
 		process.env.INPUT_INTERMEDIARY_BUILD_FLAGS = 'INPUT_INTERMEDIARY_BUILD_FLAGS';
@@ -42,9 +47,12 @@ describe('Main script', () => {
 			return Promise.resolve();
 		});
 
+		const intermediary_tag = `${process.env.INPUT_INTERMEDIARY_IMAGE_URL}:${process.env.INPUT_INTERMEDIARY_HASH}`;
+
 		require('../scripts/main').then(() => {
 			expect(exit_stub.calledOnceWith(0)).to.be.true;
-			expect(github_stub.callCount).to.be.eql(2);
+			expect(github_output_stub.calledOnceWith('intermediary_tag', intermediary_tag)).to.be.true;
+			expect(github_state_stub.calledOnceWith('INTERMEDIARY_TAG', intermediary_tag)).to.be.true;
 			done();
 		}).catch(e => done(e)).finally(() => {
 			docker_run_stub.restore();
@@ -52,9 +60,10 @@ describe('Main script', () => {
 	});
 
 	it('Should run pull/build commands computing special cache first', done => {
+		// This is the difference between both tests
+		process.env.INPUT_COMPUTE_DOCKERFILE_HASH = 'true';
 		process.env.INPUT_DOCKERFILE_PATH = 'INPUT_DOCKERFILE_PATH';
 		process.env.INPUT_INTERMEDIARY_HASH = 'INPUT_INTERMEDIARY_HASH';
-		process.env.INPUT_COMPUTE_DOCKERFILE_HASH = 'true';
 		process.env.INPUT_INTERMEDIARY_IMAGE_AS = 'INPUT_INTERMEDIARY_IMAGE_AS';
 		process.env.INPUT_INTERMEDIARY_IMAGE_URL = 'INPUT_INTERMEDIARY_IMAGE_URL';
 		process.env.INPUT_INTERMEDIARY_BUILD_FLAGS = 'INPUT_INTERMEDIARY_BUILD_FLAGS';
@@ -72,11 +81,12 @@ describe('Main script', () => {
 		`;
 
 		// Combined hash is 416036f94321ab167e7f991ad847c97c
+		const computed_hash = '416036f94321ab167e7f991ad847c97c';
 
 		const readfile_stub = Sinon.stub(fs, 'readFile').callsFake(() => Promise.resolve(fake_dockerfile));
 
 		const commands = [
-			`docker pull ${process.env.INPUT_INTERMEDIARY_IMAGE_URL}:416036f94321ab167e7f991ad847c97c || docker build --no-cache --target ${process.env.INPUT_INTERMEDIARY_IMAGE_AS} -t ${process.env.INPUT_INTERMEDIARY_IMAGE_URL}:416036f94321ab167e7f991ad847c97c ${process.env.INPUT_INTERMEDIARY_BUILD_FLAGS} ${process.env.INPUT_DOCKERFILE_PATH}`,
+			`docker pull ${process.env.INPUT_INTERMEDIARY_IMAGE_URL}:${computed_hash} || docker build --no-cache --target ${process.env.INPUT_INTERMEDIARY_IMAGE_AS} -t ${process.env.INPUT_INTERMEDIARY_IMAGE_URL}:${computed_hash} ${process.env.INPUT_INTERMEDIARY_BUILD_FLAGS} ${process.env.INPUT_DOCKERFILE_PATH}`,
 			process.env.INPUT_FINAL_IMAGE_BUILD_COMMAND
 		];
 
@@ -87,9 +97,12 @@ describe('Main script', () => {
 			return Promise.resolve();
 		});
 
+		const intermediary_tag = `${process.env.INPUT_INTERMEDIARY_IMAGE_URL}:${computed_hash}`;
+
 		require('../scripts/main').then(() => {
 			expect(exit_stub.calledOnceWith(0)).to.be.true;
-			expect(github_stub.callCount).to.be.eql(2);
+			expect(github_output_stub.calledOnceWith('intermediary_tag', intermediary_tag)).to.be.true;
+			expect(github_state_stub.calledOnceWith('INTERMEDIARY_TAG', intermediary_tag)).to.be.true;
 			done();
 		}).catch(e => {
 			console.error(e);
